9f930fde4b1b02654cafc1bbdd87c3bbf79728dd
João Pedro Medeiros <jpmdrsdev@gmail.com>
2024-08-31 00:07:54 -0300

    feat: creating form

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react'
+import './style.css'
+import Trash from '../../assets/trash.svg'
+/*import api from '../../services/api.js'*/
+
+function Home() {
+  let users = []
+
+  async function getUsers() {
+    users = await api.get('/usuarios')
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+
+  return (
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder="Nome" name='nome' type="text" />
+        <input placeholder="Idade" name='idade' type="number" />
+        <input placeholder="Email" name='email' type="email" />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export default Home
